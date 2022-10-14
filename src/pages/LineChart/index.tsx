5642da62b688ff9880f9fee9d5bb593724d04553
sam-20 <botwesamuel192@gmail.com>
2022-10-14 02:16:39 +0100

    linechart

--- src/pages/LineChart/index.tsx
+++ src/pages/LineChart/index.tsx
@@ -15,43 +15,43 @@ const data = [
     name: "Page A",
     uv: 4000,
     pv: 2400,
-    // amt: 2400,
+    amt: 2400,
   },
   {
     name: "Page B",
     uv: 3000,
     pv: 1398,
-    // amt: 2210,
+    amt: 2210,
   },
   {
     name: "Page C",
     uv: 2000,
     pv: 9800,
-    // amt: 2290,
+    amt: 2290,
   },
   {
     name: "Page D",
     uv: 2780,
     pv: 3908,
-    // amt: 2000,
+    amt: 2000,
   },
   {
     name: "Page E",
     uv: 1890,
     pv: 4800,
-    // amt: 2181,
+    amt: 2181,
   },
   {
     name: "Page F",
     uv: 2390,
     pv: 3800,
-    // amt: 2500,
+    amt: 2500,
   },
   {
     name: "Page G",
     uv: 3490,
     pv: 4300,
-    // amt: 2100,
+    amt: 2100,
   },
 ];
 
@@ -90,6 +90,7 @@ function Linechart() {
             activeDot={{ r: 8 }}
           />
           <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="amt" stroke="red" />
         </LineChart>
       </ResponsiveContainer>
     </div>
